8de5acda5defbbb950e3d42dd709ee8e23a67d8a
s.bagherzadeh <s.bagherzadeh@PC_634>
2024-12-16 09:35:46 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("please enter a number");
+var UserNumber = (Console.ReadLine());
+var Number = int.Parse(UserNumber);
+if (Number < 10)
+{
+    Console.WriteLine("NoK");
+}
+else
+{
+    Console.WriteLine("OK");
+}
+Console.ReadKey();
